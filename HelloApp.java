0c6dec5ee0af45ddf9419b6a04edc8bdfbf2daef
dm3557-ai <dm3557@srmist.edu.in>
2026-03-18 19:44:54 +0530

    UC3:Added default message

--- HelloApp.java
+++ HelloApp.java
@@ -2,10 +2,9 @@ public class HelloApp {
     public static void main(String[] args) {
 
         if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
+            System.out.println("Hello, " + args[0] + "!");
         } else {
-            System.out.println("Hello, World!");
+            System.out.println("Hello, User! Welcome!");
         }
 
     }
