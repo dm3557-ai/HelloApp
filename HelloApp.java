0243848fc4c25b58bf0b1063610ba406f4814c12
dm3557-ai <dm3557@srmist.edu.in>
2026-03-18 15:51:58 +0530

    Added UC2: Hello with command-line argument

--- HelloApp.java
+++ HelloApp.java
@@ -2,9 +2,11 @@ public class HelloApp {
     public static void main(String[] args) {
 
         if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
         } else {
-            System.out.println("Please provide a name");
+            System.out.println("Hello, World!");
         }
+
     }
 }
