fc10287278c2bf6e630a09dd0b1fd760bc83a41a
dm3557-ai <dm3557@srmist.edu.in>
2026-03-18 20:04:59 +0530

    UC5:Enhanced for loop for multiple arguments

--- HelloApp.java
+++ HelloApp.java
@@ -4,12 +4,9 @@ public class HelloApp {
         if (args.length > 0) {
             System.out.print("Hello, ");
 
-            for (int i = 0; i < args.length; i++) {
-                System.out.print(args[i]);
-
-                if (i < args.length - 1) {
-                    System.out.print(", ");
-                }
+            // Enhanced for loop
+            for (String name : args) {
+                System.out.print(name + " ");
             }
 
             System.out.println("!");
